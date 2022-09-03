de7e51ca134d3c1c2841cfb285e99cbefcadc340
Debasu <bayleyegn100@gmail.com>
2022-09-03 17:25:31 -0400

    Adds additional functionality during on click listener.

--- LoginFormValidation/app/src/main/java/com/yedebkid/loginformvalidaton/MainActivity2.kt
+++ LoginFormValidation/app/src/main/java/com/yedebkid/loginformvalidaton/MainActivity2.kt
@@ -7,105 +7,155 @@ import android.os.Bundle
 import android.util.Patterns
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputLayout
 import com.yedebkid.loginformvalidaton.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
     private val binding by lazy {
         ActivityMain2Binding.inflate(layoutInflater)
     }
+    private val sharedPreferences: SharedPreferences by lazy {
+        baseContext.getSharedPreferences("SavedEmail", MODE_PRIVATE)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        inputFocusListener()
-        btnEnabler()
+
+        emailFocusListener()
+        passwordFocusedListener()
+        password2FocusedListener()
+
+        binding.button.setOnClickListener { submitForm() }
     }
-    private val sharedPreferences: SharedPreferences by lazy { //object
-        baseContext.getSharedPreferences("My_Share_prefs", MODE_PRIVATE)
-    }
-    override fun onResume() {
-        super.onResume()
-        binding.button.setOnClickListener {
-        binding.button.setBackgroundColor(Color.parseColor("#06AA0B"))
-            val mString =
-                binding.emailEditView.text.toString() //this is the data that I am going to save
-            sharedPreferences.edit().apply {
-                putString("My_Email", mString)
-                apply()
-            }
-            Toast.makeText(baseContext, "You Successfully Created Your Account.", Toast.LENGTH_LONG).show()
-            finish()
-        }
-    }
+
     //To save data during screen rotation
     override fun onSaveInstanceState(outState: Bundle) {//bundle object help us to put a data
         super.onSaveInstanceState(outState)
         outState.putInt("Age", 33)
-
     }
+
     //To restore data during screen rotation
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         val data = savedInstanceState.getInt("Age")
-
     }
 
-    private fun inputFocusListener(){
-        var validEmail = false; var validPassword = false
+    private fun submitForm() {
+        binding.emailTextView.helperText = validEmail()
+        binding.textInputLayout.helperText = validPassword()
+        binding.textInputLayout2.helperText = validPassword2()
+
+        val validEmail = binding.emailTextView.helperText == null
+        val validPassword = binding.textInputLayout.helperText == null
+        val validConfrirmPassword = binding.textInputLayout2.helperText == null
+
+        if (validEmail && validPassword && validConfrirmPassword) {
+            //this is the data that I am going to save
+            resetForm()
+            sharedPreferences.edit().apply {
+                putString(binding.emailEditView.text.toString(), binding.emailEditView.text.toString())
+                apply()
+            }
+        } else
+            invalidForm()
+    }
+
+    private fun invalidForm() {
+
+        var message = " "
+        if (binding.emailTextView.helperText != null)
+            message += "\n\nEmail: " + binding.emailTextView.helperText
+        if (binding.textInputLayout.helperText != null)
+            message += "\n\nPassword: " + binding.textInputLayout.helperText
+        if (binding.textInputLayout2.helperText != null)
+            message += "\n\nPassword Matches: " + binding.textInputLayout2.helperText
+        AlertDialog.Builder(this)
+            .setTitle("Invalid Form")
+            .setMessage(message)
+            .setPositiveButton("Ok") { _, _ ->
+
+            }
+            .show()
+    }
+
+    private fun resetForm() {
+        var message = "Thanks for creating your account. \nEmail: " + binding.emailEditView.text
+        AlertDialog.Builder(this)
+            .setTitle("Form Submited")
+            .setMessage(message)
+            .setPositiveButton("Ok") { _, _ ->
+                binding.emailEditView.text = null
+                binding.passwordeditview.text = null
+                binding.confirmEditText.text = null
+
+                binding.emailTextView.helperText = getString(R.string.Required)
+                binding.textInputLayout.helperText = getString(R.string.Required)
+                binding.textInputLayout2.helperText = getString(R.string.Required)
+            }.show()
+    }
+
+    private fun emailFocusListener() {
         binding.emailEditView.setOnFocusChangeListener { _, focused ->
-            if(!focused) {
-                validEmail = validateEmail()
-            }
-        }
-        binding.passwordeditview.setOnFocusChangeListener { _, focused ->
-            if(!focused) {
-                validPassword = validatePassword()
-            }
-        }
-        binding.confirmEditText.setOnFocusChangeListener { _, focused ->
-            if(!focused) {
-                validPassword = validatePassword()
+            if (!focused) {
+                binding.emailTextView.helperText = validEmail()
             }
         }
     }
 
-    private fun validateEmail (): Boolean {
+    private fun validEmail(): String? {
         val emailText = binding.emailEditView.text.toString()
-        return if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()){
-            "This field is required and, your email should include @ and .com, try again".apply {
-                binding.emailEditView.error = this
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
+            return " The Email you entered is not valid, try again."
+        }
+        return null
+    }
+
+    private fun passwordFocusedListener() {
+        binding.passwordeditview.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                binding.textInputLayout.helperText = validPassword()
             }
-            false
-        } else {
-            true
         }
     }
 
-    private fun validatePassword (): Boolean {
-        val passText = binding.passwordeditview.text.toString()
-        val confText = binding.confirmEditText.text.toString()
-        return if (!passText.matches("^(?=.{8,})(?=.*[a-z])(?=.*[A-Z]).*\$".toRegex())) {
-            "Try again, see the criteria below".apply {
-                binding.passwordeditview.error = this
+    private fun validPassword(): String? {
+        val passwordText = binding.passwordeditview.text.toString()
+        if (passwordText.length < 8) {
+            return "Password length must be more than 8 character."
+        }
+        if (!passwordText.matches("^(?=.{8,})(?=.*[a-z])(?=.*[A-Z]).*\$".toRegex())) {
+            return "Password does not full fill minimum requirements. Refer the criteria below."
+        }
+
+        return null
+    }
+
+    private fun password2FocusedListener() {
+        binding.confirmEditText.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                binding.textInputLayout2.helperText = validPassword()
             }
-            false
-        } else if (passText != confText) {
-            "Password does not match, try again".apply {
-                binding.confirmEditText.error = this
-            }
-            false
-        } else {
-            btnEnabler()
-            binding.passwordeditview.error = null
-            binding.confirmEditText.error = null
-            true
         }
     }
-    fun btnEnabler (){
-        if(validateEmail() && validatePassword()){
-            binding.button.isEnabled = true
+
+    private fun validPassword2(): String? {
+        val password1Text = binding.passwordeditview.text.toString()
+        val password2Text = binding.confirmEditText.text.toString()
+        if (password2Text != password1Text) {
+            return "Password does not match."
         }
+        return null
     }
+
+
 }
+
+
 
